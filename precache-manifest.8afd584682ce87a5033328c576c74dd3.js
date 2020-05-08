82c282f808e3877fedcadd2fa73d5fd977340ad3
natimks <natalia.inf123@gmail.com>
2020-05-08 15:28:09 -0300

    Updates

---
+++ precache-manifest.8afd584682ce87a5033328c576c74dd3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eda84f111d01650dd25ecd9a376fd6a5",
+    "url": "/contacts-app/index.html"
+  },
+  {
+    "revision": "23d214aeeaf529f0a760",
+    "url": "/contacts-app/static/css/main.de015842.chunk.css"
+  },
+  {
+    "revision": "4014a602d7dac12d9abd",
+    "url": "/contacts-app/static/js/2.f99cbc26.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/contacts-app/static/js/2.f99cbc26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23d214aeeaf529f0a760",
+    "url": "/contacts-app/static/js/main.34e1e3e0.chunk.js"
+  },
+  {
+    "revision": "e36db61e71a0f62d7a11",
+    "url": "/contacts-app/static/js/runtime-main.6d88eb97.js"
+  },
+  {
+    "revision": "82a7c06238e831cd8f70bf1182156c2e",
+    "url": "/contacts-app/static/media/logo.82a7c062.svg"
+  }
+]);
